a2bb1f816e3e9c132dd334d7ad2b08fb4989cdf0
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-10-10 12:26:33 -0300

    Calculadora finalizada

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ function clean(){
 }
 function calcular(){
     let resultado = document.getElementById('display').innerHTML;
-    document.getElementById('display').innerHTML = eval(resultado);
+    if(resultado){
+        document.getElementById('display').innerHTML = eval(resultado);
+    }else{
+        document.getElementById('display').innerHTML = "Nada";
+    }
 }
 function calcLog10(){
     let numero = parseFloat(document.getElementById('display').innerHTML);
@@ -24,4 +28,28 @@ function calcLogBaseNatural(){
     let numero = parseFloat(document.getElementById('display').innerHTML);
     let resultado = Math.log(numero);
     document.getElementById('display').innerHTML = resultado;
+}
+function calcRaiz(){
+    let numero = parseFloat(document.getElementById('display').innerHTML);
+    let resultado = Math.sqrt(numero);
+    document.getElementById('display').innerHTML = resultado;
+}
+function potencia2(){
+    let numero = parseFloat(document.getElementById('display').innerHTML);
+    let resultado = numero ** 2;
+    document.getElementById('display').innerHTML = resultado;
+}
+function fatorial(){
+    let numero = parseFloat(document.getElementById('display').innerHTML);
+
+    if(numero < 0){
+        document.getElementById('display').innerHTML = -1;
+    }else if(numero == 0){
+        document.getElementById('display').innerHTML = 1;
+    }else{
+        for(let i = numero-1; i >= 1; i--){
+            numero *= i;
+        }
+        document.getElementById('display').innerHTML = numero;
+    }
 }
